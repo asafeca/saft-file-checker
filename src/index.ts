d0e59f999d242a0649e2f54165ab545843a65d7a
Adelino Safeca <safeetyy@gmail.com>
2022-05-30 10:40:51 +0100

    code refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,16 @@
 import { SaftFileParser } from "./domain/SaftFileParser";
-import * as fs from "fs"
 import { SaftFileParserImpl } from "./domain/SaftFileParserImpl";
+import {IDataResult } from "./components/commons/iresult";
 
-export function parseFile() {
+ function parseFile(content: Uint8Array):IDataResult {
 
-  let content = fs.readFileSync("test.xml");
+  //let content = fs.readFileSync("test.xml");
   const dataView = new Uint8Array(content);
   let parser: SaftFileParser = new SaftFileParserImpl();
 
-  let result = parser.parse(dataView)
-  console.log(`The result is --> ${result.message}`);
+  return  parser.parse(dataView)
 
 
 }
 
-parseFile()
-
-/*
-
-const specials = new RegExp("^[^<>%$*,;'^']*$|^[^a-zA-Z]*$")
-const regex = new RegExp(specials)
-
-const isCorrect =  new RegExp("^[^a-zA-Z]*$").test("52") && new RegExp("^[^<>%$*,;'^'@'{}'%''-''+'#`]*$").test("52")
-const right = new Date("2021-10-26T09:51:24")
-
-console.log(right)
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-//https://medium.com/openmindonline/js-monday-17-publishing-a-typescript-library-59dd8200f80d
+module.exports = parseFile
