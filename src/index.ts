aeddc45e58469f9d7950d2b46b93a389faa4a869
Adelino Safeca <safeetyy@gmail.com>
2022-05-30 11:15:15 +0100

    .gitignore

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { SaftFileParser } from "./domain/SaftFileParser";
 import { SaftFileParserImpl } from "./domain/SaftFileParserImpl";
 import {IDataResult } from "./components/commons/iresult";
 
- function parseFile(content: Uint8Array):IDataResult {
+ export function parseFile(content: Uint8Array):IDataResult {
 
   //let content = fs.readFileSync("test.xml");
   const dataView = new Uint8Array(content);
