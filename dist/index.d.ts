2d08395991b2160ee6589067e9050f4cf721f386
Adelino Safeca <safeetyy@gmail.com>
2022-05-30 11:16:59 +0100

    npm build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
-export {};
+import { IDataResult } from "./components/commons/iresult";
+export declare function parseFile(content: Uint8Array): IDataResult;
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.parseFile = void 0;
 var SaftFileParserImpl_1 = require("./domain/SaftFileParserImpl");
 function parseFile(content) {
     //let content = fs.readFileSync("test.xml");
@@ -7,4 +8,5 @@ function parseFile(content) {
     var parser = new SaftFileParserImpl_1.SaftFileParserImpl();
     return parser.parse(dataView);
 }
+exports.parseFile = parseFile;
 module.exports = parseFile;
